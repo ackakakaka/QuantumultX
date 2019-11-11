f8c6ecc86602ccecb4ae6ee361f6e0f6775db101
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2019-11-12 02:22:36 +0800

    Update zhibo-tv.js

--- zhibo-tv.js
+++ zhibo-tv.js
@@ -1,9 +1,5 @@
 var body = $response.body;
-var url = $request.url;
-const path = "/room/get-room-info-v430/";
-let obj = JSON.parse(body);
-if (url.indexOf(path) != -1) {
-	obj.data.userHas = 1;
-	body = JSON.stringify(obj);
- }
+var obj = JSON.parse(body);
+obj.userHas = 1;
+body = JSON.stringify(obj);
 $done({body});
