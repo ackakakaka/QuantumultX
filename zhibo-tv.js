32ecaed91a5717867d7635ec2116615638a7a1c3
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2019-11-12 16:42:04 +0800

    Update zhibo-tv.js

--- zhibo-tv.js
+++ zhibo-tv.js
@@ -1,9 +1,19 @@
-/**
-  * @supported CCA72BB5
-  */
 
 var body = $response.body;
+var url = $resquest.url;
 var obj = JSON.parse(body);
-obj.data.anchor.userHas = 1;
-body = JSON.stringify(obj);
+const path1 = "/room/get-room-info-v430";
+const path2 = "/video/appnesting-detail";
+if (url.indexOf(path1) != -1){
+ let obj = JSON.parse(body);
+ obj.data.anchor.userHas = 1;
+ body = JSON.stringify(obj);
+}
+
+if (url.indexOf(path2) != -1){
+ let obj = JSON.parse(body);
+ obj.data.userHas = 1;
+ body = JSON.stringify(obj);
+}
+
 $done({body});
