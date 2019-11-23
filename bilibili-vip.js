29118f4da9621aedb4d9237f55f578a50cb2ce98
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2019-11-24 00:19:08 +0800

    Update bilibili-vip.js

--- bilibili-vip.js
+++ bilibili-vip.js
@@ -1,9 +1,7 @@
 /*
-
 #QX版 哔哩哔哩大会员
-https:\/\/api\.bilibili\.com\/pgc\/player\/api\/playurl url 302 https://bili.miao.best/geturl/maom/
-https:\/\/api\.bilibili\.com\/pgc\/view\/app\/season url script-response-body bilibili-vip.js
-
+^https:\/\/api\.bilibili\.com\/pgc\/player\/api\/playurl url 302 https://bili.miao.best/geturl/maom/
+^https:\/\/api\.bilibili\.com\/pgc\/view\/app\/season url script-response-body https://raw.githubusercontent.com/ackakakaka/QuantumultX/master/bilibili-vip.js
 */
 
 let obj = JSON.parse($response.body);
