24e45ade262b7199f8e5ec1c53ec02b0c1058b71
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2019-12-12 21:00:10 +0800

    Add files via upload

---
+++ FaceApp.js
@@ -0,0 +1,27 @@
+/*
+
+http-response ^https?:\/\/api\.faceapp\.io(.*)\/api\/v.*\/auth\/user\/credentials requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/FaceApp.js
+
+[MITM]
+hostname = api.faceapp.io
+*/
+
+let headers = $response.headers; 
+let obj = JSON.parse($response.body);
+ 
+obj = {
+ "user": null,
+ "user_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzZXNzaW9uX2lkIjoiIiwiZGV2aWNlX2lkIjoiNzE2MDdDMzEtRDE2My00NjZFLTkzRjctNEEzMkU4RERCRTNEIiwiZXhwIjoxNTc2MjMzMDUxLCJzdWJzY3JpcHRpb24iOnsic3Vic2NyaXB0aW9uX2V4cCI6MTU3NjIzMzA1MSwiY3VzdG9tZXJfaWQiOiJGMi9mS2R5cWgwRVdXK3N4V21BaEZBRG9iTHdzVXVQZnBOZlUzRjlzK0djPSIsInByb2R1Y3RfaWQiOiIxIn19.1NT8jfiajbakI1s_nohsic5r47DKxqeua4RvqH7Zp0MXlWVWEgQzK6LvCxcxxzk0buzbn4iJxYh7x2XhLos0KA",
+ "user_token_lifetime": 86400,
+ "subscription_apple": {
+  "subscription_exp": 1576233051,
+  "product_id": "1"
+ },
+ "subscription_google": null
+};
+ 
+$done({
+    body: JSON.stringify(obj),
+    headers: headers,
+    status: 200,
+});
