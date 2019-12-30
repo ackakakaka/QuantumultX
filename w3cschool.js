da4eca64f232b5cd38448b8890aa1fdc0fc384fb
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2019-12-30 18:39:30 +0800

    Update w3cschool.js

--- w3cschool.js
+++ w3cschool.js
@@ -4,11 +4,10 @@ w3cschool VIP UNLOCK
 ^http?:\/\/appapi\.w3cschool\.cn\/api\/myapp\/isLogin url script-response-body https://raw.githubusercontent.com/ackakakaka/QuantumultX/master/w3cschool.js
 */
 
-var body = $response.body;
- let obj = JSON.parse(body);
- obj.viptype = "2";
- obj.viplevel = "1";
- obj.vipexpire = "2030-10-10 10-10-10";
- body = JSON.stringify(obj);
-
+var obj = $response.body;
+obj = {
+ "viptype" = "2",
+ "viplevel" = "1",
+ "vipexpire" = "2030-10-10 10-10-10",
+};
 $done({body});
