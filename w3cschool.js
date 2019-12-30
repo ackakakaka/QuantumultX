fde1bedc37537ca5ae163782c0dd13236b46f744
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2019-12-30 23:21:34 +0800

    Update w3cschool.js

--- w3cschool.js
+++ w3cschool.js
@@ -5,20 +5,13 @@ w3cschool VIP UNLOCK
 */
 
 var body = $response.body;
-<<<<<<< HEAD
 var url = $request.url;
 const path1 = "/api/myapp/isLogin";
 if (url.indexOf(path1) != -1){
  let obj = JSON.parse(body);
-=======
-let obj = JSON.parse(body);
->>>>>>> 7ee748f3e749332d2dad07bd84d2deb481f3d1ff
  obj.viptype = "2";
  obj.viplevel = "1";
  obj.vipexpire = "2030-10-10 10-10-10";
  body = JSON.stringify(obj);
-<<<<<<< HEAD
 }
-=======
->>>>>>> 7ee748f3e749332d2dad07bd84d2deb481f3d1ff
 $done({body});
