98375e2fd27469b2e325f5e5f9693b66b05ccaa3
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2019-12-31 00:27:40 +0800

    Update w3cschool.js

--- w3cschool.js
+++ w3cschool.js
@@ -5,10 +5,10 @@ w3cschool VIP UNLOCK
 */
 
 var body = $response.body;
- let obj = JSON.parse(body);
- obj["viptype"] = "2";
- obj["viplevel"] = "1";
- obj["vipexpire"] = "2030-10-10 10-10-10";
+var obj = JSON.parse(body);
+ obj.viptype = "2";
+ obj.viplevel = "1";
+ obj.vipexpire = "2030-10-10 10-10-10";
  body = JSON.stringify(obj);
 }
 $done({body});
