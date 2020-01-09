eadadf3b706702363f090ce60c7a736c147a39ed
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2020-01-09 18:47:40 +0800

    Update workingcopy.js

--- workingcopy.js
+++ workingcopy.js
@@ -1,5 +1,7 @@
 /*
 ^https:\/\/education\.github\.com\/api\/user$ url script-response-body https://raw.githubusercontent.com/ackakakaka/QuantumultX/master/workingcopy.js
+
+MITM = education.github.com
 */
 
 $done({body: '{"student": true}'})
