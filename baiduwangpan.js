7f5681fdd05a3459c85d31ff53f9d4f2d90da519
ackakakaka <31911347+ackakakaka@users.noreply.github.com>
2020-01-09 18:45:50 +0800

    Update baiduwangpan.js

--- baiduwangpan.js
+++ baiduwangpan.js
@@ -1,11 +1,8 @@
 /*
 Baidu netdisc unlocks online video play speed.
 
-Surge4.0:
-http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
-
 QX1.0.0:
-https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/ackakakaka/QuantumultX/master/baiduwangpan.js
 
 MITM = pan.baidu.com
 */
